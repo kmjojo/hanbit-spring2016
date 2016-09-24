a37879c70ce9d2b0d6c1bbc17b2e17d0e447f936
1027 <1027@hb2004>
2016-09-24 17:16:03 +0900

    logger

--- src/main/java/com/hanbit/spring/FirstBean.java
+++ src/main/java/com/hanbit/spring/FirstBean.java
@@ -1,25 +1,25 @@
 package com.hanbit.spring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 //이 어노테이션을 사용하면 xml config에 bean을 등록하지 않아도 됨
 @Component
 public class FirstBean {
+	
+	private static final Logger logger = LoggerFactory.getLogger(FirstBean.class);
 
-	private SecondBean secondBean; 
+	// first와 second 간의 의존성
+	@Autowired
+	private SecondBean secondBean;  
 	
 	private int value;
 	
-
-	// FirstBean과 SecondBean의 관계를 알려줌
-	@Autowired
-	public void setSecondBean(SecondBean secondBean) {
-		this.secondBean = secondBean;
-	}
-	
 	public void callSecondBean() {
-		System.out.println(value++);
+		logger.debug("logger test");
+		logger.info("" + value++);
 		secondBean.printName();
 	}  
 
--- src/main/java/com/hanbit/spring/HelloSpring.java
+++ src/main/java/com/hanbit/spring/HelloSpring.java
@@ -2,13 +2,16 @@ package com.hanbit.spring;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.util.Log4jConfigurer;
 
 public class HelloSpring {
 	
 
 
-	public static void main(String[] args) {
-		System.out.println("HelloSpring");
+	public static void main(String[] args) throws Exception {
+		
+		// 이 내용은 config에 들어갈듯
+		Log4jConfigurer.initLogging("classpath:config/log4j.xml");
 		
 		/*
 		Dependency dp = new Dependency();
