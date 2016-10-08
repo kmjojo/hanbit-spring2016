3aaecce5b70f62c0c66e4485fe4f448cb52af5c6
1027 <1027@hb2004>
2016-10-08 18:18:57 +0900

    session 다시 작성해야 함

--- src/main/java/com/hanbit/spring/core/session/SessionAspect.java
+++ src/main/java/com/hanbit/spring/core/session/SessionAspect.java
@@ -3,6 +3,7 @@ package com.hanbit.spring.core.session;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
 /**
@@ -16,6 +17,14 @@ public class SessionAspect {
 
 	@Around("@annotation(com.hanbit.spring.core.session.SigninRequired)")
 	public Object checkSignIn(ProceedingJoinPoint pjp) throws Throwable {
+		
+		
+		// 지금 실행되고 있는 메소드에 대한 정보
+		//pjp.getSignature();
+		
+		// MethodSignature 로 형변환함으로써 더 많은 정보를 알아낼 수 있음?
+		//MethodSignature signature = (MethodSignature) pjp.getSignature();
+		
 		
 		
 		Object returnValue = pjp.proceed();
